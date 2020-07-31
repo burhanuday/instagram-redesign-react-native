49763b1c37b74b39090fc63333f2f82b4d4cbeed
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2020-07-31 18:47:22 +0530

    added real image urls

--- src/mock/posts.js
+++ src/mock/posts.js
@@ -1,9 +1,14 @@
+const baseImageURL = "https://picsum.photos/400";
+const baseAvatarUrl = "https://picsum.photos/100";
+// `${baseImageURL}?random=${Math.random() * 100}`
+// `${baseAvatarUrl}?random=${Math.random() * 100}`
+
 export const posts = [
   {
     id: 1,
     name: "Brannon Stonebridge",
     time_posted: "5:46 PM",
-    url: "http://dummyimage.com/240x240.bmp/5fa2dd/ffffff",
+    url: `${baseImageURL}?random=${Math.random() * 100}`,
     caption: "Phasellus in felis. Donec semper sapien a libero. Nam dui.",
     likeCount: 179,
     commentCount: 39,
@@ -23,14 +28,14 @@ export const posts = [
         username: "rlukovic2",
       },
     ],
-    avatar: "http://dummyimage.com/163x195.png/cc0000/ffffff",
+    avatar: `${baseAvatarUrl}?random=${Math.random() * 100}`,
     username: "bstonebridge0",
   },
   {
     id: 2,
     name: "Ricardo Devin",
     time_posted: "8:49 AM",
-    url: "http://dummyimage.com/243x179.bmp/5fa2dd/ffffff",
+    url: `${baseImageURL}?random=${Math.random() * 100}`,
     caption:
       "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
     likeCount: 103,
@@ -42,14 +47,14 @@ export const posts = [
         username: "ccalam0",
       },
     ],
-    avatar: "http://dummyimage.com/250x234.png/ff4444/ffffff",
+    avatar: `${baseAvatarUrl}?random=${Math.random() * 100}`,
     username: "rdevin1",
   },
   {
     id: 3,
     name: "Pamella Fitter",
     time_posted: "12:32 PM",
-    url: "http://dummyimage.com/209x139.jpg/ff4444/ffffff",
+    url: `${baseImageURL}?random=${Math.random() * 100}`,
     caption:
       "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
     likeCount: 379,
@@ -71,14 +76,14 @@ export const posts = [
         username: "upresser2",
       },
     ],
-    avatar: "http://dummyimage.com/158x194.bmp/5fa2dd/ffffff",
+    avatar: `${baseAvatarUrl}?random=${Math.random() * 100}`,
     username: "pfitter2",
   },
   {
     id: 4,
     name: "Norina Sills",
     time_posted: "6:55 AM",
-    url: "http://dummyimage.com/181x192.jpg/ff4444/ffffff",
+    url: `${baseImageURL}?random=${Math.random() * 100}`,
     caption:
       "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
     likeCount: 1792,
@@ -110,14 +115,14 @@ export const posts = [
         username: "tmccrow4",
       },
     ],
-    avatar: "http://dummyimage.com/107x118.bmp/ff4444/ffffff",
+    avatar: `${baseAvatarUrl}?random=${Math.random() * 100}`,
     username: "nsills3",
   },
   {
     id: 5,
     name: "Chloette Paulack",
     time_posted: "7:27 AM",
-    url: "http://dummyimage.com/111x135.jpg/5fa2dd/ffffff",
+    url: `${baseImageURL}?random=${Math.random() * 100}`,
     caption:
       "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
     likeCount: 1017,
@@ -148,14 +153,14 @@ export const posts = [
         username: "scharge4",
       },
     ],
-    avatar: "http://dummyimage.com/218x105.png/ff4444/ffffff",
+    avatar: `${baseAvatarUrl}?random=${Math.random() * 100}`,
     username: "cpaulack4",
   },
   {
     id: 6,
     name: "Myrtle Antognazzi",
     time_posted: "12:00 PM",
-    url: "http://dummyimage.com/141x217.png/dddddd/000000",
+    url: `${baseImageURL}?random=${Math.random() * 100}`,
     caption: "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
     likeCount: 1267,
     commentCount: 39,
@@ -166,14 +171,14 @@ export const posts = [
         username: "kkestell0",
       },
     ],
-    avatar: "http://dummyimage.com/250x141.bmp/cc0000/ffffff",
+    avatar: `${baseAvatarUrl}?random=${Math.random() * 100}`,
     username: "mantognazzi5",
   },
   {
     id: 7,
     name: "Zena Handasyde",
     time_posted: "2:19 AM",
-    url: "http://dummyimage.com/147x178.jpg/ff4444/ffffff",
+    url: `${baseImageURL}?random=${Math.random() * 100}`,
     caption:
       "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
     likeCount: 1396,
@@ -203,99 +208,7 @@ export const posts = [
         username: "pavery4",
       },
     ],
-    avatar: "http://dummyimage.com/101x160.jpg/dddddd/000000",
+    avatar: `${baseAvatarUrl}?random=${Math.random() * 100}`,
     username: "zhandasyde6",
-  },
-  {
-    id: 8,
-    name: "Cyndia Purle",
-    time_posted: "9:24 AM",
-    url: "http://dummyimage.com/175x142.png/dddddd/000000",
-    caption: "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
-    likeCount: 1617,
-    commentCount: 66,
-    comments: [
-      {
-        comment:
-          "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-        username: "strulocke0",
-      },
-      {
-        comment:
-          "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
-        username: "jbarhems1",
-      },
-      {
-        comment: "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
-        username: "sbelbin2",
-      },
-      {
-        comment:
-          "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
-        username: "rbilsborrow3",
-      },
-    ],
-    avatar: "http://dummyimage.com/196x159.jpg/dddddd/000000",
-    username: "cpurle7",
-  },
-  {
-    id: 9,
-    name: "Marcie Jolin",
-    time_posted: "12:12 AM",
-    url: "http://dummyimage.com/170x144.bmp/ff4444/ffffff",
-    caption:
-      "Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.",
-    likeCount: 1602,
-    commentCount: 33,
-    comments: [
-      {
-        comment:
-          "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
-        username: "rhendricks0",
-      },
-      {
-        comment:
-          "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.",
-        username: "dvanson1",
-      },
-      {
-        comment:
-          "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
-        username: "hmeeks2",
-      },
-      {
-        comment: "Sed ante. Vivamus tortor. Duis mattis egestas metus.",
-        username: "jfrushard3",
-      },
-      {
-        comment:
-          "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-        username: "rdamarell4",
-      },
-    ],
-    avatar: "http://dummyimage.com/122x230.bmp/cc0000/ffffff",
-    username: "mjolin8",
-  },
-  {
-    id: 10,
-    name: "Bettine Wynne",
-    time_posted: "2:41 PM",
-    url: "http://dummyimage.com/200x181.jpg/dddddd/000000",
-    caption:
-      "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
-    likeCount: 947,
-    commentCount: 54,
-    comments: [
-      {
-        comment: "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
-        username: "tlitton0",
-      },
-      {
-        comment: "Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
-        username: "brame1",
-      },
-    ],
-    avatar: "http://dummyimage.com/175x121.png/cc0000/ffffff",
-    username: "bwynne9",
   },
 ];
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, StatusBar, FlatList, ScrollView } from "react-native";
+import { StyleSheet, StatusBar, FlatList } from "react-native";
 import { Surface } from "react-native-paper";
 
 import Card from "@components/Card";
@@ -10,7 +10,7 @@ import { posts } from "../mock/posts";
 
 const Posts = () => (
   <FlatList
-    data={[{ stories: true }, ...posts]}
+    data={[{ stories: true, id: 9999 }, ...posts]}
     renderItem={({ item, index }) =>
       index === 0 ? <Stories /> : <Card openScreen={true} post={item} />
     }
